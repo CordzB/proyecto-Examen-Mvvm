573d3686fdd50712ace20a90b79ae5d67078ca44
CordzB <167384843+CordzB@users.noreply.github.com>
2024-12-11 19:27:28 -0600

    Add project files.

---
+++ proyecto Examen-Mvvm/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace proyecto_Examen_Mvvm
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ proyecto Examen-Mvvm/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace proyecto_Examen_Mvvm
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
